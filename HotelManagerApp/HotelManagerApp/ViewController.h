b32f57a3172f85a1522a56fd1d788bd1357a74bb
matthew weintrub <heytrub@gmail.com>
2015-12-01 13:19:03 -0800

    first commit: init projecT

---
+++ HotelManagerApp/HotelManagerApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelManagerApp
+//
+//  Created by Matthew Weintrub on 12/1/15.
+//  Copyright © 2015 matthew weintrub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
