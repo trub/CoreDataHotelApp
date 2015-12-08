fed0888ac6694f0cfc0b253060fa5e1a0d6defb5
matthew weintrub <heytrub@gmail.com>
2015-12-08 13:12:49 -0800

    creates tableView for availability controller

--- HotelManagerApp/HotelManagerApp/AvailabilityViewController.m
+++ HotelManagerApp/HotelManagerApp/AvailabilityViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "AvailabilityViewController.h"
+#import "Hotel.h"
+#import "Room.h"
 
-@interface AvailabilityViewController ()
+@interface AvailabilityViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) NSArray *datasource;
 
 @end
 
@@ -16,9 +21,78 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self setupTableView];
+
 }
+
+-(void)loadView {
+    [super loadView];
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+
+
+- (void)setupTableView {
+    self.tableView = [[UITableView alloc]init];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.view addSubview:self.tableView];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    
+    NSLayoutConstraint *leading = [NSLayoutConstraint constraintWithItem:self.tableView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0];
+    NSLayoutConstraint *top = [NSLayoutConstraint constraintWithItem:self.tableView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0];
+    NSLayoutConstraint *trailing = [NSLayoutConstraint constraintWithItem:self.tableView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0.0];
+    NSLayoutConstraint *bottom = [NSLayoutConstraint constraintWithItem:self.tableView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0];
+    
+    leading.active = YES;
+    top.active = YES;
+    trailing.active = YES;
+    bottom.active = YES;
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return [self.datasource count];
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    
+    Room *room = (Room *)self.datasource[indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"Room: %i (%i beds, $%0.2f per night)", room.number.intValue, room.beds.intValue, room.rate.floatValue];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return  210.0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UIImage *headerImage = [UIImage imageNamed:@"Hotel"];
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:headerImage];
+    
+    imageView.frame = CGRectMake(0.0, 0.0, CGRectGetWidth(self.view.frame), 210.0);
+    imageView.contentMode = UIViewContentModeScaleAspectFill;
+    imageView.clipsToBounds = YES;
+    return  imageView;
+}
+
+
 
 @end
